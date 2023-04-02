efa97e0a445e18d38a420e2c752d97977ddb5ff5
naruto <niceddeveloper@gmail.com>
2023-04-03 00:56:29 +0600

    enums

--- resources/views/pages/search-method.blade.php
+++ resources/views/pages/search-method.blade.php
@@ -14,14 +14,14 @@
 
     <form action="{{ route('searcher', $method) }}" method="GET" class="flex justify-center mt-6">
         <div class="flex items-center">
-            <input name="query"  type="text" class="px-4 py-2 border rounded-l-md w-64" placeholder="Search...">
+            <input name="query" type="text" class="px-4 py-2 border rounded-l-md w-64" placeholder="Search...">
             <button type="submit" class="ml-4 px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600">Search</button>
         </div>
     </form>
 
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-2">
+            <div class="flex flex-wrap -m-2 mb-8">
                 @foreach($orders as $order)
                     <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                         <div class="h-full flex items-center border-gray-200 bg-white border p-4 rounded-lg">
@@ -29,13 +29,13 @@
                             <div class="flex-grow">
                                 <h2 class="text-gray-900 title-font font-medium">id: {{ $order->id }}</h2>
                                 <p class="text-gray-500">{{ $order->title }}</p>
-                                <p class="text-gray-900">Client: {{ $order->client }}</p>
+                                <p class="text-gray-900">{{ $order->client }}</p>
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
+            {{ $orders->links() }}
         </div>
-        {{ $orders->links() }}
     </section>
 @endsection
